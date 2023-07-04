df67348036c45158b80a18784c9a0911bd489496
akirsch1 <andre.kirsch@fh-bielefeld.de>
2023-07-04 16:33:06 +0200

    Added function to use as a python module

--- scripts/load_yaml.py
+++ scripts/load_yaml.py
@@ -3,24 +3,27 @@
 import sys
 import yaml
 
-def print_usage():
-    print("usage: load_yaml.py [filename] [prefix(optional)]")
-    print("       After execution, source the output to add the variables")
-    print("       to the environment.")
-    exit(-1)
+def _print_usage():
+    if __name__ == "__main__":
+        print("usage: load_yaml.py [filename] [prefix(optional)]")
+        print("       After execution, source the output to add the variables")
+        print("       to the environment.")
+        exit(-1)
+    else:
+        raise RuntimeError("Failed to load yaml file.")
 
-def get_filename():
+def _get_filename(argv):
     # Get filename from argv. Appends file extension if necessary.
     # Returns filename. Does not return on error.
-    if len(sys.argv) == 2 or len(sys.argv) == 3:
-        filename = sys.argv[1]
+    if len(argv) == 2 or len(argv) == 3:
+        filename = argv[1]
 
         if not filename.endswith(".yml"):
             filename += ".yml"
         return filename
-    print_usage()
+    _print_usage()
 
-def read_yaml(filename):
+def _read_yaml(filename):
     # Reads yaml data from given filename.
     # Returns yaml data object. Does not return on error.
     try:
@@ -29,62 +32,77 @@ def read_yaml(filename):
                 return yaml.safe_load(file)
             except yaml.YAMLError as err:
                 print("Invalid yaml file")
-                print_usage()
+                _print_usage()
     except FileNotFoundError as err:
         print("File %s not found" % filename)
-        print_usage()
+        _print_usage()
 
-def get_env_prefix(filename):
+def _get_env_prefix(filename, argv):
     # Returns either the argument specified or converts the filename
     # with path and file extension to the prefix.
     # In both cases, the prefix is converted to uppercase.
-    if len(sys.argv) == 3:
-        return sys.argv[2].upper()
+    if len(argv) == 3 and argv[2] is not None:
+        return argv[2].upper()
 
     size = len(filename)
     start = size - filename[size:0:-1].index('/', 0, size)
     end = size - filename[size:0:-1].index('.', 0, size) - 1
     return filename[start:end].upper()
 
-def print_env_variables(yaml_data, prefix):
+def _print_env_variables(env_variables):
     # Print environment variables to shell so they can be
     # loaded by a shell script to the parent shell environment.
+    for key in env_variables:
+        print("export %s=%s" % (key, env_variables[key]))
+
+def _get_env_variables(yaml_data, prefix):
+    # Create environment variable dictionary from yaml data
+    env_vars = {}
     for key in yaml_data:
         value = yaml_data[key]
         if isinstance(value, str):
-            print("export %s_%s=%s" % (prefix, key.upper(),
-                    value.replace(" ", "\ ")))
+            env_vars["%s_%s" % (prefix, key.upper())] = value.replace(" ", "\ ")
         elif isinstance(value, int) or isinstance(value, float):
-            print("export %s_%s=%s" % (prefix, key.upper(), value))
+            env_vars["%s_%s" % (prefix, key.upper())] = value
         elif isinstance(value, list):
             if isinstance(value[0], str) or isinstance(value, int) \
                     or isinstance(value, float):
                 # TODO: This is currently just a workaround that might
                 # not work.
-                print("export %s_%s=%s" % (prefix, key.upper(),
-                        "\ ".join(value)))
+                env_vars["%s_%s" % (prefix, key.upper())] = "\ ".join(value)
             elif isinstance(value[0], dict):
                 keys = []
                 for v in value:
                     keys.extend(v.keys())
-                    print_env_variables(v, "%s_%s" % (prefix, key.upper()))
-                print("export %s_%s=%s" % (prefix, key.upper(),
-                        "\ ".join(keys)))
+                    env_vars.update(_get_env_variables(v, "%s_%s" % (prefix, key.upper())))
+                env_vars["%s_%s" % (prefix, key.upper())] = "\ ".join(keys)
             else:
-                print("echo \"Unknown type type(%s)\"" % type(value))
+                print("echo \"Unknown type (%s)\"" % type(value))
         elif isinstance(value, dict):
-            #print("export %s_%s=%s" % (prefix, key.upper(),
-            #        "\ ".join(value.keys())))
-            print_env_variables(value, "%s_%s" % (prefix, key.upper()))
+            env_vars.update(_get_env_variables(value, "%s_%s" % (prefix, key.upper())))
         else:
             # Printing this with echo so that it does not break the other
             # exports.
-            print("echo \"Unknown type type(%s)\"" % type(value))
+            print("echo \"Unknown type (%s)\"" % type(value))
+    return env_vars
 
-def main():
-    filename = get_filename()
-    yaml_data = read_yaml(filename)
-    print_env_variables(yaml_data, get_env_prefix(filename))
+
+def parse_env_variables(path, prefix=None):
+    # Parse environment variables from the given path
+    # Set the prefix for a different value than the filename
+    argv = ["load_yaml", path, prefix]
+    filename = _get_filename(argv)
+    yaml_data = _read_yaml(filename)
+    env_prefix = _get_env_prefix(filename, argv)
+    env_vars = _get_env_variables(yaml_data, env_prefix)
+    _print_env_variables(env_vars)
+
+def _main():
+    filename = _get_filename(sys.argv)
+    yaml_data = _read_yaml(filename)
+    env_prefix = _get_env_prefix(filename, sys.argv)
+    env_vars = _get_env_variables(yaml_data, env_prefix)
+    _print_env_variables(env_vars)
 
 if __name__ == "__main__":
-    main()
+    _main()
