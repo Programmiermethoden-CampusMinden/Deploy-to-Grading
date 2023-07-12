524cc6b50f187e84ec3bddd451fac24eb27aead6
akirsch1 <andre.kirsch@fh-bielefeld.de>
2023-07-12 08:54:42 +0200

    Added comments

--- scripts/checkout_due_date.sh
+++ scripts/checkout_due_date.sh
@@ -7,11 +7,13 @@ EOM
     exit -1
 }
 
+# Check if $1 is a valid datetime string
 if ! date -d $1 2>: 1>:
 then
     usage
 fi
 
+# Find first commit before the given datetime
 COMMIT_ID=$(git log --before=$1 -n 1 | grep commit | cut -d' ' -f2)
 
 if [ -z "$COMMIT_ID" ]
@@ -19,6 +21,7 @@ then
     echo "Error: No commit found before $1"
     exit -1
 else
+    # Checkout given commit id
     git checkout $COMMIT_ID 2>: 1>:
 fi
 
