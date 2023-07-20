cc695e44328a83b1878cf1d3181f2e05ad1ccd51
akirsch1 <andre.kirsch@fh-bielefeld.de>
2023-07-20 09:48:38 +0200

    Added more detailed documentation and other small changes

--- scripts/checkout_due_date.sh
+++ scripts/checkout_due_date.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
+# Finds the last commit of a git repository thas was pushed before
+# a given datetime. The datetime is typically the due date of the
+# current assignment. Make sure to execute the script inside a folder
+# that contains a git repository.
+#
+# usage: checkout_due_date.sh [datetime]
+#   Params:
+#   datetime    A valid datetime format is YYYY-MM-DDThh:mm. See git
+#               log for more information on valid datetime formats.
+#
+# This script is step 2 in the Deploy-to-Grading pipeline that is
+# executed for the whole assignment.
+#
+
 usage() {
     # Print usage information
     cat <<EOM
 usage: ./$(basename $0) [datetime]
-  Checks out the last commit before the given. This script needs to be
-  run in a folder containing a git repository.
+  Checks out the last commit before the given datetime. This script
+  needs to be run in a folder containing a git repository.
 
   Params:
   datetime    See git log documentation for valid datetime formats.
 EOM
-    exit -1
 }
 
 # Check if $1 is a valid datetime string
 if ! date -d $1 2>: 1>:
 then
     usage
+    exit -1
 fi
 
 # Find first commit before the given datetime
@@ -26,6 +40,7 @@ if [ -z "$COMMIT_ID" ]
 then
     echo "Error: No commit found before $1"
     usage
+    exit -1
 fi
 
 # Checkout given commit id
@@ -36,7 +51,7 @@ if [ ! $? -eq 0 ]
 then
     echo "Error: Failed to checkout commit $COMMIT_ID"
     usage
-else
-    echo "Successfully loaded commit with id $COMMIT_ID"
-fi
+    exit -1
+
+echo "Successfully loaded commit with id $COMMIT_ID"
 
