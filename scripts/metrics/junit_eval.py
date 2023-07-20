b9a0d764b3cfcd883d0a09c5f266999d56418e77
akirsch1 <andre.kirsch@fh-bielefeld.de>
2023-07-20 11:50:20 +0200

    Small documentation update

--- scripts/metrics/junit_eval.py
+++ scripts/metrics/junit_eval.py
@@ -20,8 +20,8 @@
 #   %TASKNAME%_METRICS_JUNIT_POINTS_PER_TEST environment variable is not
 #   set.
 #
-# This script is step 6 in the Deploy-to-Grading pipeline that is
-# executed for every task that includes the junit metric.
+# This script is part of step 6 in the Deploy-to-Grading pipeline that
+# is executed for every task that includes the junit metric.
 #
 
 import os
