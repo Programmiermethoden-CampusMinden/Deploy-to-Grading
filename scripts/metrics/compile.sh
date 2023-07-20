3b5adf2141941dbf2d5c1bfa82414dcf8b9a9ad1
akirsch1 <andre.kirsch@fh-bielefeld.de>
2023-07-20 14:22:54 +0200

    Removed usage function in bash script

--- scripts/metrics/compile.sh
+++ scripts/metrics/compile.sh
@@ -13,14 +13,6 @@
 
 DIR=build/results
 
-usage() {
-    # Print usage information
-    cat <<EOM
-usage: ./$(basename $0)
-  Runs gradle to compile the java code and saves the results.
-EOM
-}
-
 if [ ! -d "$DIR" ]
 then
     mkdir -p "$DIR"
