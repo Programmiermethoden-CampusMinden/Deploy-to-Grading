2c34d30c2835df429d6af7a33ba0e10d6e2c1815
akirsch1 <andre.kirsch@fh-bielefeld.de>
2023-07-27 14:19:01 +0200

    Add create_artifact.sh

---
+++ scripts/create_artifact.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Copies the results folder from the indiviual tasks to the assignments
+# result folder. The assignment and task configuration are also copied.
+# Furthermore, this script creates a zip archive to distibute the results
+# (e.g. as an artifact in the GitHub Action).
+#
+# usage: create_artifact.sh
+#
+# This script is part of step 7 in the Deploy-to-Grading pipeline.
+#
+
+RESULTS_DIR=results
+METRICS_DIR=metrics
+
+# Copy task.yml and the results of every metric to the assignments
+# result folder for every task
+for TASK in $ASSIGNMENT_TASKS
+do
+    SRC_DIR=$TASK/build/results
+    TARGET_DIR=$RESULTS_DIR/$TASK
+
+    if [ ! -d "$TARGET_DIR/$METRICS_DIR/" ]
+    then
+        mkdir -p "$TARGET_DIR/$METRICS_DIR/"
+    fi
+
+    cp -r $SRC_DIR/* $TARGET_DIR/$METRICS_DIR/
+    cp $TASK/task.yml $TARGET_DIR
+done
+
+# Copy assignment.yml
+cp assignment.yml $RESULTS_DIR/
+
+# Create zip archive from results folder
+zip results.zip -r $RESULTS_DIR
